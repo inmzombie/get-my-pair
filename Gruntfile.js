97dc2a59cddc10d8a518d7271623161df17a6781
mr29a <dbarros@hydras.com.ar>
2015-10-05 08:28:31 -0300

    Added shufle from lodash library for easier reading of code and finish app

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,12 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
     var transportConfig = grunt.file.readJSON('./config/transportConfig.json');
     var recipientsConfig = grunt.file.readJSON('./config/recipientsConfig.json');
     var coupleBuilder = require('./src/js/coupleBuilder.js');
     var couples = coupleBuilder(recipientsConfig.recipients);
+    grunt.file.write('./tmp/pair-data.json', JSON.stringify(couples));
     grunt.initConfig({
          nodemailer: {
             options: {
@@ -16,11 +17,19 @@ module.exports = function(grunt) {
                 recipients: recipientsConfig.recipients,
                 },
                 external: {
-                    src: ['./src/template/index.html']
+                    src: ['./tmp/src/template/table.html']
                 }
-            }
+         },
+         assemble: {
+            options: {
+                data: ['./tmp/pair-data.json']
+              },
+              emailTemplate: {
+                data: ['./tmp/pair-data.json'],
+                src: ['./src/template/table.hbs'],
+                dest: './tmp'
+              }
+         }
     });
-    grunt.registerTask('default', [], function() {
-
-    });
+    grunt.registerTask('default', ['assemble:emailTemplate', 'nodemailer:external']);
 };
--- src/js/coupleBuilder.js
+++ src/js/coupleBuilder.js
@@ -1,5 +1,4 @@
-module.exports = function(names) {
-       return names.slice(0).sort(function(){ return Math.random()-0.5 }).map(function(name, index, arr){
-               return name + " gets " + arr[(index+1)%arr.length];
-                 });
+module.exports = function(devList) {
+    var _ = require('lodash');
+    return _.chunk(_.shuffle(devList), 2);
 }
